2ca7b2078453daba968cd03a14965ab2a99e60b7
신예찬 <oolo9876@yonsei.ac.kr>
2024-08-18 20:03:47 +0900

    4.1

--- houses/admin.py
+++ houses/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.db import models
+from .models import House
 
-# Register your models here.
-class House(models.Model):
 
-    """Model Definition for Houses"""
-
-    name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
-    description = models.TextField()
-    address = models.CharField(max_length=140)
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    pass
